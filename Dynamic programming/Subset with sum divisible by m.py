33f2afb326aaa902ce491c3aadd9a2a34f1d6edd
Flare7 <64733540+Flare7@users.noreply.github.com>
2020-10-25 00:07:54 +0200

    Add files via upload

---
+++ Dynamic programming/Subset with sum divisible by m.py
@@ -0,0 +1,36 @@
+def modularSum(arr, n, m): 
+  
+    if (n > m): 
+        return True
+    #will create list of m matrix with value False
+    DP = [False for i in range(m)] 
+  
+    for i in range(n): 
+
+        #It will return True value if index 0 of DP is True       
+        if (DP[0]): 
+            return True
+        
+        #
+        temp = [False for i in range(m)] 
+  
+        for j in range(m): 
+          
+            if (DP[j] == True): 
+              
+                if (DP[(j + arr[i]) % m] == False): 
+
+                    temp[(j + arr[i]) % m] = True
+              
+        for j in range(m): 
+            if (temp[j]): 
+                DP[j] = True
+  
+        DP[arr[i] % m] = True
+      
+    return DP[0] 
+
+arr = [1, 7,8,8] 
+n = len(arr) 
+m = 5
+print("YES") if(modularSum(arr, n, m)) else print("NO")
